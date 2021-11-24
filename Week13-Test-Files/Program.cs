e42c714da30da2181f9650065496e6b28f9c9216
karlartur <89841872+karlartur@users.noreply.github.com>
2021-11-24 09:38:23 +0200

    Add files via upload

---
+++ Week13-Test-Files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace Week13_Test_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\Samples";
+
+            Console.WriteLine("Enter directory  name:");
+            string userFolder = Console.ReadLine();
+
+            string newDirectoryFullPath = $@"{rootDirectory}\{userFolder}";
+            bool directoryExists = Directory.Exists(newDirectoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userFolder} exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryFullPath);
+                Console.WriteLine($"Directory {userFolder} has been created.");
+            }
+
+            Directory.CreateDirectory(newDirectoryFullPath);
+
+        }
+    }
+}
